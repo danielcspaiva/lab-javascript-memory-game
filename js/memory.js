7e3c0dd91d563b9067b3789a6a7695b6c704d32e
Daniel Paiva <danielcspaiva@gmail.com>
2020-03-19 16:43:45 -0300

    constructor, shuffleCards and CheckIfPair methods

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,32 @@
 class MemoryGame {
   constructor(cards){
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  
+  shuffleCards() {
+    let j, aux = 0
+    for (let i = this.cards.length - 1; i >= 1; i -= 1) {
+      j = Math.round(Math.random() * i)
+      aux = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = aux;
+    }
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    }
+    return false;
+  }
+
+  isFinished() {
+    
+  }
 }
